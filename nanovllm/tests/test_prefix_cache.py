24a9020a2d56b1f3fa15b2138bbd7d904b2950db
StevenLiuShiqi <sl3646@cornell.edu>
2026-05-04 19:57:03 -0400

    fix the seq.block_size to fixed 4

--- nanovllm/tests/test_prefix_cache.py
+++ nanovllm/tests/test_prefix_cache.py
@@ -5,6 +5,7 @@ bm = BlockManager(num_blocks=20, block_size=4)
                     
 # First request: 12 token, using 3 blocks                      
 seq_a = Sequence([1, 2, 3, 4,  5, 6, 7, 8,  9, 10, 11, 12])
+seq_a.block_size = 4
 bm.allocate(seq_a)                                                
 print("After seq_a allocate:", bm.stats())                      
 print("seq_a block_table:", seq_a.block_table)                    
@@ -12,6 +13,7 @@ print("seq_a num_cached_tokens:", seq_a.num_cached_tokens)
                                                                   
 # Second request: first 8 tokens identical with the first req
 seq_b = Sequence([1, 2, 3, 4,  5, 6, 7, 8,  99, 100, 101, 102])
+seq_b.block_size = 4
 bm.allocate(seq_b)                                                
 print("\nAfter seq_b allocate:", bm.stats()) 
 print("seq_b block_table:", seq_b.block_table)                    
