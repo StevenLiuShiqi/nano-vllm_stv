5b6428854e9f064701bc6db3e0ff2d7200172572
root <sl3646@cornell.edu>
2026-05-17 10:13:10 +0800

    example

--- example.py
+++ example.py
@@ -31,7 +31,7 @@ def main():
     print(attn.get_stats())
     print(blkmngr.get_stats())
     attn.clear_stats()
-    blkmngr.clear_stats
+    blkmngr.clear_stats()
 
     outputs2 = llm.generate(prompts[:2], sampling_params)
     # attn = llm.model_runner.model.model.layers[0].self_attn.attn            
